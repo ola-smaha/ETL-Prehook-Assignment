9aeda9077ef4d78733656c4a6bc7087a21ca1419
ola-smaha <141483688+ola-smaha@users.noreply.github.com>
2023-09-12 13:12:59 +0300

    edited

--- prehook.py
+++ prehook.py
@@ -4,18 +4,18 @@ from lookups import ErrorHandling, PreHookSteps, SQLTablesToReplicate, InputType
 from logging_handler import show_error_message
 
 def execute_folder(directory_path, input_file_type, db_session, input_schema=None):
-    files = [file for file in os.listdir(directory_path) if file.endswith(input_file_type.value)] # edited InputTypes in lookups
+    files = [file for file in os.listdir(directory_path.value) if file.endswith(input_file_type.value)] # edited InputTypes in lookups
     sorted_files =  sorted(files)    
     if input_file_type == InputTypes.SQL:
         for sql_file in sorted_files:
-            with open(os.path.join(directory_path,sql_file), 'r') as sql_file:
+            with open(os.path.join(directory_path.value,sql_file), 'r') as sql_file:
                 sql_query = sql_file.read()
                 return_val = execute_query(db_session= db_session, query= sql_query)
                 if not return_val == ErrorHandling.NO_ERROR:
                     raise Exception(f"{PreHookSteps.EXECUTE_SQL_QUERY.value} = SQL File Error on SQL FILE = " +  str(sql_file))
     elif input_file_type == InputTypes.CSV:
         for csv_file in sorted_files:
-            csv_df = return_data_as_df(file_executor=os.path.join(directory_path,csv_file), input_type=InputTypes.CSV)
+            csv_df = return_data_as_df(file_executor=os.path.join(directory_path.value,csv_file), input_type=InputTypes.CSV)
             old_columns = csv_df.columns
             new_columns = []
             for column in old_columns:
@@ -24,7 +24,7 @@ def execute_folder(directory_path, input_file_type, db_session, input_schema=Non
                 new_columns.append(column)
             csv_df.columns = new_columns
             table_name = csv_file[:-4] # generating table name by removing '.csv'
-            csv_query = return_create_statement_from_df(csv_df,schema_name=input_schema,table_name=table_name)
+            csv_query = return_create_statement_from_df(csv_df,schema_name=input_schema.value,table_name=table_name)
             return_val = execute_query(db_session= db_session, query= csv_query)
             if not return_val == ErrorHandling.NO_ERROR:
                 raise Exception(f"{PreHookSteps.EXECUTE_CSV_QUERY.value} = CSV File Error on CSV FILE = {str(csv_file)}" )
@@ -58,12 +58,12 @@ def create_sql_staging_tables(db_session, source_name):
 def execute_prehook(directory_path,schema):
     try:
         db_session = create_connection()
-        if directory_path == DirectoryPaths.SQL_FOLDER.value:
+        if directory_path == DirectoryPaths.SQL_FOLDER:
             # Step 1:
             execute_folder(directory_path, InputTypes.SQL , db_session) 
             # Step 2 getting dvd rental staging:
             create_sql_staging_tables(db_session,SourceName.DVD_RENTAL.value)
-        elif directory_path == DirectoryPaths.CSV_FOLDER.value:
+        elif directory_path == DirectoryPaths.CSV_FOLDER:
             # Step 1:
             execute_folder(directory_path, InputTypes.CSV , db_session, input_schema=schema) 
             # Step 2 getting powerbi staging tables:
